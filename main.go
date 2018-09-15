2e92676aa91e0e09a2649c6cb3e90a2d478c8ccc
Ben Hartshorne <ben@honeycomb.io>
2018-09-15 08:02:21 -0700

    remove all instrumentation for demonstration

--- main.go
+++ main.go
@@ -8,13 +8,10 @@ import (
 	"math"
 	"math/rand"
 	"net/http"
-	"os"
 	"strings"
 	"sync"
 	"time"
 
-	beeline "github.com/honeycombio/beeline-go"
-	"github.com/honeycombio/beeline-go/wrappers/hnynethttp"
 	"github.com/honeycombio/leakybucket"
 )
 
@@ -50,35 +47,19 @@ type app struct {
 
 func main() {
 
-	wk := os.Getenv("HONEYCOMB_APIKEY")
-	var useStdout bool
-	if wk == "" {
-		useStdout = true
-	}
-	// Initialize beeline. The only required field is WriteKey.
-	beeline.Init(beeline.Config{
-		WriteKey:    wk,
-		Dataset:     "beeline-example",
-		ServiceName: "rlp",
-		// In no writekey is configured, send the event to STDOUT instead of Honeycomb.
-		STDOUT: useStdout,
-	})
-
 	client := http.DefaultClient
-	client.Transport = hnynethttp.WrapRoundTripper(http.DefaultTransport)
 	a := &app{
 		client:      client,
 		rateLimiter: make(map[string]*leakybucket.Bucket),
 	}
 
-	http.HandleFunc("/", hnynethttp.WrapHandlerFunc(a.proxy))
+	http.HandleFunc("/", a.proxy)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
 func (a *app) proxy(w http.ResponseWriter, req *http.Request) {
 	var rateKey string
 	forwarded := req.Header.Get("X-Forwarded-For")
-	beeline.AddField(req.Context(), "forwarded_for_incoming", forwarded)
 	if forwarded == "" {
 		rateKey = strings.Split(req.RemoteAddr, ":")[0]
 	} else {
@@ -86,14 +67,9 @@ func (a *app) proxy(w http.ResponseWriter, req *http.Request) {
 	}
 
 	// check rate limits
-	beeline.AddField(req.Context(), "rate_limit_key", rateKey)
 	hitCapacity := a.shouldRateLimit(req.Method, rateKey)
 	if hitCapacity != nil {
-		beeline.AddField(req.Context(), "error", "rate limit exceeded")
-		ctx, span := beeline.StartSpan(req.Context(), "wait")
-		defer span.Send()
 		sleepTime := math.Abs(waitBaseTime + (rand.NormFloat64()*waitStdDev + waitRange))
-		beeline.AddField(ctx, "wait_time", sleepTime)
 		// sleep a random amount in the range (waitBaseTime to waitBaseTime+waitRange)
 		// aka 100-600ms
 		time.Sleep(time.Duration(sleepTime) * time.Millisecond)
@@ -112,8 +88,6 @@ func (a *app) proxy(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		w.WriteHeader(http.StatusServiceUnavailable)
 		io.WriteString(w, `{"error":"failed to create downstream request"}`)
-		beeline.AddField(req.Context(), "error", err)
-		beeline.AddField(req.Context(), "error_detail", "failed to create downstream request")
 		return
 	}
 	// add context to propagate the beeline trace
@@ -127,14 +101,11 @@ func (a *app) proxy(w http.ResponseWriter, req *http.Request) {
 	} else {
 		downstreamReq.Header.Set("X-Forwarded-For", req.RemoteAddr)
 	}
-	beeline.AddField(req.Context(), "forwarded_for_outgoing", downstreamReq.Header.Get("X-Forwarded-For"))
 	// call the downstream service
 	resp, err := a.client.Do(downstreamReq)
 	if err != nil {
 		w.WriteHeader(http.StatusServiceUnavailable)
 		io.WriteString(w, `{"error":"downstream target unavailable"}`)
-		beeline.AddField(req.Context(), "error", err)
-		beeline.AddField(req.Context(), "error_detail", "downstream target unavailable")
 		return
 	}
 	// ok, we got a response, let's pass it along
